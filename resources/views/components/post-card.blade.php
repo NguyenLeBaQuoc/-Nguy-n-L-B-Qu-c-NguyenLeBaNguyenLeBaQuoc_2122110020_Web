d002956656d801ba96a4c11a8f279c1edb0f6647
NguyenLeBaQuoc <baby.quocnguyen2@gmail.com>
2024-05-27 07:43:45 +0700

    Update 22/5/24

---
+++ resources/views/components/post-card.blade.php
@@ -0,0 +1,12 @@
+<div class="row mb-2">
+    <div class="col-5">
+        <img class="img-fluid w-100" src="images/ao-cup-nguc.jpg" alt="">
+    </div>
+    <div class="col-7">
+        <h6>15/08/2021 - bởi: Ego Creative</h6>
+        <h4 class="ct">Một cô nàng ăn mặc chất, ngoài khả năng bắt trend còn tuân thủ 4</h4>
+        <p>Những bộ đồ ôm sát body giúp tôn lên đường cong nhưng mặc một
+            chiếc đầm bó chật quá mức hoặc chiếc quần jeans siêu skinny với mong muốn khoe
+        </p>
+    </div>
+</div>
---
+++ resources/views/components/product-card.blade.php
@@ -0,0 +1,40 @@
+<div class="col-md">
+    <div class="product-item">
+        <div class="product-item">
+            <a href="product-detail">
+
+
+                <img class="img-fluid" src="images/ao-cup-nguc.jpg" alt=""> </a>
+        </div>
+        <div class="product-name">
+            <h4 class="fs-5 py-1">ÁO POLO PHỐI KHÓA CỔ</h4>
+        </div>
+        <div class="product-price">
+            <div class="row">
+                <div class="col-6">
+                    <strong class="text-danger fs-5">399.000 <sup>đ</sup></strong>
+                </div>
+                <div class="col-6 text-end">
+                    <del>460.000 <sup>đ</sup></del>
+                </div>
+            </div>
+        </div>
+        <div class="product-cart-hearth-eye my-2">
+            <div class="row">
+                <div class="col-8">
+                    <a class="btn btn-sm btn-success form-control">Thêm vào giỏ hàng</a>
+                </div>
+                <div class="col-2">
+                    <button class="btn btn-sm btn-info">
+                        <i class="fa-regular fa-heart"></i>
+                    </button>
+                </div>
+                <div class="col-2">
+                    <button class="btn btn-sm btn-info">
+                        <i class="fa-regular fa-eye"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
